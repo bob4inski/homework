10e94f0e253863923a6c10773aff87ff05108d04
Bob4inski <robertstryzhak@gmail.com>
2021-09-08 15:06:04 +0300

    дз

---
+++ ss.py
@@ -0,0 +1,16 @@
+'''def shaitan_mashina(a,b):
+    if a ==b:
+        return ('a = b')
+    if a > b:
+        return ('a > b')
+    else:
+        return ('a < b') 
+a = 5 
+b = 10
+print(shaitan_mashina(a,b))'''
+a = []
+for i in range(0,50,2):
+    a.append(i)
+print(a)
+a = 'bib bam boom '
+print(a*2)
