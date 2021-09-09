ea3df08784ff4556484156391295b5a6240f3a37
Bob4inski <robertstryzhak@gmail.com>
2021-09-09 11:43:02 +0300

    пшшш

--- ss.py
+++ ss.py
@@ -1,16 +1,14 @@
-'''def shaitan_mashina(a,b):
-    if a ==b:
-        return ('a = b')
-    if a > b:
-        return ('a > b')
-    else:
-        return ('a < b') 
-a = 5 
-b = 10
-print(shaitan_mashina(a,b))'''
-a = []
-for i in range(0,50,2):
-    a.append(i)
-print(a)
-a = 'bib bam boom '
-print(a*2)
+def initials_PRO(names):
+    b = len(names)
+    new_names = []
+    for i in range(b):
+        a,b,c= names[i].split(' ')
+        a +=' ' + b[0]+ ' ' + c[0]
+        new_names.append(a)
+    return new_names
+
+
+spisok=['СТрыжак ро
+берт андреевич','попов ростислав валерьевич','меняйло катерина андреевна','загребин керил максимович','герман стрыжак андреевич']
+
+print(initials_PRO(spisok))
