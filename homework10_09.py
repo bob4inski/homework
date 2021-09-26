241dff5a4b91a4caebd2eb8263acb4afcc7fbf73
Bob4inski <robertstryzhak@gmail.com>
2021-09-26 17:35:02 +0300

    это домашка!

---
+++ homework10_09.py
@@ -0,0 +1,22 @@
+def show(func):
+    def new_func(*args, **kwargs):
+        print('Running function: ', func.__name__)
+        print('Positional arguments are: ', args)
+        print('Keyword arguments are: ', kwargs)
+        
+    return new_func
+
+@show
+def initials_PRO(names):
+    b = len(names)
+    new_names = []
+    for i in range(b):
+        a,b,c= names[i].split(' ')
+        a +=' ' + b[0]+ ' ' + c[0]
+        new_names.append(a)
+    return new_names
+
+
+spisok=['СТрыжак роберт андреевич','попов ростислав валерьевич','меняйло катерина андреевна','загребин керил максимович','герман стрыжак андреевич']
+
+print(initials_PRO(spisok))
